6b7d2e313638997afe0443805f2faebf2e10dcdd
Danila Bladyko <danila.bladyko@stark.games>
2024-04-17 16:37:22 +0300

    Добавил возможность запускать игру с любой сцены

---
+++ Assets/Scripts/Runtime/Infrastructure/Bootstrap/GameBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Runtime.Constants;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace Runtime.Infrastructure.Bootstrap
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ProjectContext projectContext = FindObjectOfType<ProjectContext>();
+
+            if (projectContext is null)
+            {
+                SceneManager.LoadScene(SceneNames.Bootstrap);
+            }
+        }
+    }
+}
