d0e0951688e170b5ededf9f46484d710d9290863
Danila Bladyko <danila.bladyko@stark.games>
2024-04-21 15:18:54 +0300

    add_brick удалил лишнее

--- Assets/Scripts/Runtime/Infrastructure/SlicableObjects/SlicableObjectView.cs
+++ Assets/Scripts/Runtime/Infrastructure/SlicableObjects/SlicableObjectView.cs
@@ -14,17 +14,8 @@ namespace Runtime.Infrastructure.SlicableObjects
         public SpriteRenderer ShadowSprite => _shadowSprite;
         public Collider2D Collider2D => _collider2D;
 
-        private void Reset(Vector3 startPosition)
-        {
-            transform.position = startPosition;
-        }
-
         public class Pool : MonoMemoryPool<Vector3, SlicableObjectView>
         {
-            protected override void Reinitialize(Vector3 position, SlicableObjectView slicableObjectView)
-            {
-                slicableObjectView.Reset(position);
-            }
         }
     }
 }
